8df304af2b34c5451510c9b99eed096209586553
GawthamW97 <39323106+GawthamW97@users.noreply.github.com>
2018-09-04 08:31:09 +0530

    Create BankAccount.h

---
+++ BankAccount.h
@@ -0,0 +1,22 @@
+#ifndef _BANKACCOUNT
+#define _BANKACCOUNT
+
+class BankAccount {
+
+private:
+	int accountNumber;
+	double balance;
+
+public:
+	BankAccount();
+	BankAccount(int, double);
+	const double ANNUAL_RATE;
+	void setAccountNumber(int);
+	void setAccountBalance(double);
+	int getAccountNumber();
+	double getAccountBalance();
+	void enterAccountData();
+	void computeInterest(int);
+	void displayAccount();
+};
+#endif
